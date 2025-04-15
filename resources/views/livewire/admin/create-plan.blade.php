a42e2971d44b1fa6c02c0c1dfb2ac11d51033fcc
Azil6744 <developmentwithazil@gmail.com>
2025-04-15 18:01:43 +0500

    Implement user and admin creation forms with validation and success notifications

--- resources/views/livewire/admin/create-plan.blade.php
+++ resources/views/livewire/admin/create-plan.blade.php
@@ -3,21 +3,33 @@
         <div class="col-xl-12 ">
             <h6 class="mb-0 text-uppercase">Create Plan</h6>
             <hr />
-            <div class="card" wire:submit.prevent="store">
-                <form>
+            <div class="card">
+                <form  wire:submit.prevent="store">
                 <div class="card-body">
                     <label for="name" class="form-label">Name</label>
                     <input class="form-control mb-3" type="text" wire:model="name" placeholder="Name" aria-label="default input example"
                         name="name" id="name">
+                        @error('name')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                     <label for="name" class="form-label">Description</label>
                     <input class="form-control mb-3" type="text" wire:model="description" placeholder="Description"
                         aria-label="default input example" name="name" id="name">
+                        @error('description')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                     <label for="name" class="form-label">Price</label>
                     <input class="form-control mb-3" type="text"  wire:model="price" placeholder="Price"
                         aria-label="default input example" name="name" id="name">
+                        @error('price')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                     <label for="name" class="form-label">Duration</label>
                     <input class="form-control mb-3" type="text" wire:model="duration" placeholder="Duration"
                         aria-label="default input example" name="name" id="name">
+                        @error('duration')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                     <label for="name" class="form-label">Duration Unit</label>
                     <select class="form-select mb-3" id="type" wire:model="duration_unit" aria-label="Default select example">
                         <option value="" selected>Select Unit</option>
@@ -26,6 +38,9 @@
                         <option value="month">Month</option>
                         <option value="year">Year</option>
                     </select>
+                    @error('duration_unit')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                     <button type="submit" class="btn btn-light px-5">Create</button>
                 </div>
             </form>
--- resources/views/livewire/admin/create-user.blade.php
+++ resources/views/livewire/admin/create-user.blade.php
@@ -4,42 +4,33 @@
             <h6 class="mb-0 text-uppercase">Create User</h6>
             <hr />
             <div class="card">
-                <div class="card-body">
-                    <div class="mb-3" wire:submit.prevent="store">
-                        <label for="name" class="form-label">Image</label>
-                        <div class="multiple-file-upload">
-                            <input class="form-control" type="file" id="formFile">
-                        </div>
+                <form wire:submit.prevent="store">
+                    <div class="card-body">
+                        <label for="name" class="form-label">Name</label>
+                        <input class="form-control mb-3" type="text" placeholder="Name"
+                            aria-label="default input example" name="name" id="name" wire:model="name">
+                        @error('name')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                        <label for="name" class="form-label">Email</label>
+                        <input class="form-control mb-3" type="text" placeholder="Name"
+                            aria-label="default input example" name="name" id="name" wire:model="email">
+                        @error('email')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                        <label for="name" class="form-label">Password</label>
+                        <input type="password" class="form-control mb-3" id="password" wire:model="password"
+                            placeholder="Enter Password">
+                        @error('password')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                        <label for="name" class="form-label">Confirm Password</label>
+                        <input type="password" class="form-control mb-3" id="password"
+                            wire:model="password_confirmation" placeholder="Confirm Password">
+                        <button type="submit" class="btn btn-light px-5">Create</button>
                     </div>
-                    <input class="form-control mb-3" type="text" placeholder="Name" aria-label="default input example"
-                        name="name" id="name" wire:model="name">
-                    <select class="form-select mb-3" id="type" wire:model="type" aria-label="Default select example">
-                        <option value="" selected>Server Type</option>
-                        <option value="free">Free</option>
-                        <option value="premium">Premium</option>
-                    </select>
-                    <select class="form-select mb-3" id="status" wire:model="status" aria-label="Default select example">
-                        <option value="" selected>Select Status</option>
-                        <option value="active">Active</option>
-                        <option value="inactive">Inactive</option>
-                    </select>
-                    <input class="form-control mb-3" id="name" name="longitude" wire:model="longitude" type="text"
-                        placeholder="Longitude" aria-label="default input example">
-                    <input class="form-control mb-3" id="name" name="Latitude" wire:model="Latitude" type="text"
-                        placeholder="Latitude" aria-label="default input example">
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="form-custom-switch-success"
-                            wire:model="android">
-                        <label class="form-check-label" for="flexSwitchCheckDefault">Android</label>
-                    </div>
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" role="switch" type="checkbox" id="form-custom-switch-success"
-                            wire:model="ios">
-                        <label class="form-check-label" for="flexSwitchCheckDefault">IOS</label>
-                    </div>
-                    <button type="submit" class="btn btn-light px-5">Create</button>
-                </div>
+                </form>
             </div>
         </div>
-    </div>   
+    </div>
 </div>
